93029f2ec75890862f63b1a2683981751cc81eba
paul.hilzinger <paul.hilzinger@sky.uk>
2025-02-11 11:42:11 +0000

    2025-02-11. PKH. MZMOB-1012 Adding latest mzcoremob9 db configuration

--- mz_mob_data/src/sql/mz_mob_data-create.sql
+++ mz_mob_data/src/sql/mz_mob_data-create.sql
@@ -1,9 +1,9 @@
 -- mz_mob_data_owner
 
 create user mz_mob_data_owner identified by mz_mob
-default tablespace mz_mob_cdr_data
+default tablespace mz_mob_data
 temporary tablespace temp
-quota unlimited on mz_mob_cdr_data;
+quota unlimited on mz_mob_data;
 
 grant create table to mz_mob_data_owner;
 grant create trigger to mz_mob_data_owner;
@@ -18,9 +18,9 @@ grant create procedure to mz_mob_data_owner;
 -- mz_mob_data_user
 
 create user mz_mob_data_user identified by mz_mob
-default tablespace mz_mob_cdr_data
+default tablespace mz_mob_data
 temporary tablespace temp
-quota 0 on mz_mob_cdr_data;
+quota 0 on mz_mob_data;
 
 -- create mz mob data specific roles
 
